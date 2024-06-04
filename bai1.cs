59ea102c3805386b3d3241643e2970463a64ce75
hiep0311 <148854293+hiep0311@users.noreply.github.com>
2024-06-04 18:08:14 +0700

    Add files via upload

---
+++ bai1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8; //
+        Console.Write("Enter a number: ");
+        double num = double.Parse(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
---
+++ bai4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8; //
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int rows, cols;
+
+        Console.Write("Nhập số hàng: ");
+        rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhập số cột: ");
+        cols = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Mẫu ký tự sao (*):");
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= cols; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
